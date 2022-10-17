25e9d4762518077cdc25eb89e9d2199d883a2e9d
Bulavina Elena <koroleva488@mail.ru>
2022-10-17 14:10:04 +1000

    Задача 1 (коррект.)

--- Task1.py
+++ Task1.py
@@ -1,7 +1,11 @@
 # Задача 1.  Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
 
-txt = input("Введите текст через пробел:\n")
-print(f"Исходный текст: {txt}")
-find_txt = "абв"
-listFirst = [i for i in txt.split() if find_txt not in i]
-print(f'Результат: {" ".join(listFirst)}')
+
+my_text = ('Напишите абв напиабв програбвмму программу, удаляющую из этого абв текста все вабвс слова, содерабващие содержащие "абв"')
+
+def del_some_words(my_text):
+    my_text = list(filter(lambda x: 'абв' not in x, my_text.split()))
+    return " ".join(my_text)
+
+my_text = del_some_words(my_text)
+print(my_text)
